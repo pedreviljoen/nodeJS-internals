640a2d4f81af05bfd2b0ee5ed2279957ed15df8f
Pedre Viljoen <pedreviljoen@Pedres-MacBook-Pro.local>
2018-12-19 14:51:34 +0200

    Started -> Still working on it

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+    Clustering is a module that allows you to create child processes that all share server ports.
+    
+    A single instance of node is single threaded (the event loop). To take advantage of some multi-core systems,
+    you will sometimes want to launch multiple instances of your program. This is called clustering.
+*/
+
